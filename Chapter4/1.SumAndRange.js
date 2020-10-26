f7e46a8e1b0b054330dd6acdd7221f0b59f1f8e1
Amal Dusalin <adusalin@fil-it.ru>
2020-10-26 19:24:20 +0300

    Exercises 1 from chapter 4

---
+++ Chapter4/1.SumAndRange.js
@@ -0,0 +1,40 @@
+/*
+    Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior.
+*/
+
+
+const range = (from, to, step = 1) => {
+    if (step === 0) return null
+    if (from === to) return from
+
+    const [minNum, maxNum] = from > to ? [to, from] : [from, to]
+    const result = []
+
+    if (step < 0) {
+        for (let i = maxNum; i >= minNum; i += step) {
+            result.push(i)
+        }
+
+        return result
+    }
+
+    for (let i = minNum; i <= maxNum; i += step) {
+        result.push(i)
+    }
+
+    return result
+}
+
+const sum = (values) => values.reduce((acc, num) => acc + num, 0)
+
+// Your code here.
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
